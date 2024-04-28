f3b981b4dd318b65cbaf89905d452ee61dc95281
3k2ng <44631569+3k2ng@users.noreply.github.com>
2024-04-27 19:44:13 -0600

    add an example

---
+++ examples/basic_render.cpp
@@ -0,0 +1,19 @@
+#include <shrub/context.h>
+#include <shrub/window.h>
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+
+#include <tngl/texture.h>
+
+int main() {
+	shrub::Window window(800, 600, "basic render");
+	window.make_current();
+	shrub::Context::load_gl();
+	while (!window.should_close()) {
+		glClearColor(0.2F, 0.4F, 0.3F, 1.0F);
+		glClear(GL_COLOR_BUFFER_BIT);
+		window.swap_buffers();
+		window.poll_events();
+	}
+}
